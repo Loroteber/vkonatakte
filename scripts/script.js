3fdb4c7b02b708d2929ac4628f32e4fa1614d51a
Loroteber <113366081+Loroteber@users.noreply.github.com>
2025-03-05 22:38:05 +0500

    v0.0.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,17 @@
 (function() {
     'use strict';
-    function replaceFavicon() {
-        let favicon = document.querySelector('link[rel="shortcut icon"]');
-        if (favicon) {
-            favicon.href = "https://tooru.konataizumi.ru/img/konata.ico";
-        }
-    };
     const replacements = {
         "Моя Страница": "Отаку",
+        "Мои Друзья": "Мои Накамы",
         "Мои Фотографии": "Мой Фанарт",
         "Мои Видеозаписи": "Моё Аниме",
         "Мои Аудиозаписи": "Мои OST'ы",
-        "Мои Сообщения": "Мои НЯ",
+        "Мои Сообщения": "Мои НЯшки",
+        "Мои Новости": "Моя Новоc-тян",
         "Мои Заметки": "Мои Фанфики",
         "Мои Группы": "Мои Аниме-клубы",
         "Мои Ответы": "Мой Кавай",
         "Мои Настройки": "Мои НЯстройки",
-        "Поиск": "доко ни",
-        "Тип группы": "Тип клуба",
         "группы": "аниме-клубы",
         "выйти": "дзя нэ",
         "помощь": "отаковать",
@@ -26,28 +20,66 @@
         "Все сообщения": "Все НЯ",
         "Приватность": "Секреты^^",
         "Чёрный список": "Дыз Нот",
-        "время подавать напитки и вершить судьбы": "аригато! ^_^",
-        "C'mon Grab Your Friends": "аригато! ^_^",
-        "В вашем чёрном списке": "В вашей тетради смерти",
     };
-    function replaceText() {
-        const walker = document.createTreeWalker(
-            document.body,
-            NodeFilter.SHOW_TEXT,
-            null,
-            false
-        );
-        while (walker.nextNode()) {
-            const node = walker.currentNode;
-            for (const [oldText, newText] of Object.entries(replacements)) {
-                if (node.nodeValue.includes(oldText)) {
-                    node.nodeValue = node.nodeValue.replace(new RegExp(oldText, 'g'), newText);
+    function replaceTextInElements() {
+        const search_div = document.querySelector('div.header_divider_stick');
+        const search = search_div.querySelector('input');
+        search.placeholder = "доко ни?";
+        const targetElements = document.querySelectorAll('div.sidebar, div.page_header, div.tabs, div.page_yellowheader, span.post-online');
+        targetElements.forEach(function(element) {
+            const walker = document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
+            while (walker.nextNode()) {
+                const node = walker.currentNode;
+                for (const [oldText, newText] of Object.entries(replacements)) {
+                    if (node.nodeValue.includes(oldText)) {
+                        node.nodeValue = node.nodeValue.replace(new RegExp(oldText, 'g'), newText);
+                    }
                 }
             }
+        });
+    }
+    function removeHeaderNavigationItems() {
+        const headerNavigation = document.querySelector('.header_navigation');
+
+        if (headerNavigation) {
+            const items = headerNavigation.querySelectorAll('div');
+
+            items.forEach(item => {
+                if (item.textContent.trim() === "главная" || item.textContent.trim() === "Поиск") {
+                    item.remove();
+                }
+            });
+        } else {
+            console.warn('Элемент с классом header_navigation не найден.');
         }
     }
-    replaceText();
-    replaceFavicon();
-    const observer = new MutationObserver(replaceText);
+    function trueRules() {
+        if (window.location.pathname === '/terms') {
+            const customImage = document.querySelector('img.rules-image');
+            if (!customImage) {
+                const pageContent = document.querySelector('div.page_content');
+                const div = document.createElement('div');
+                const img = document.createElement('img');
+                img.src = "https://raw.githubusercontent.com/Loroteber/vkonatakte/refs/heads/main/sources/wtfisthis.jpg";
+                img.style.width = "100%";
+                img.classList.add('rules-image');
+                pageContent.prepend(img);
+            }
+    }
+    }
+    function replaceFavicon() {
+        document.querySelector('link[rel="shortcut icon"]').href = "https://raw.githubusercontent.com/Loroteber/vkonatakte/refs/heads/main/sources/favicon.ico";
+    }
+
+    const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+                removeHeaderNavigationItems();
+                replaceTextInElements();
+                replaceFavicon();
+                trueRules();
+            }
+        });
+    });
     observer.observe(document.body, { childList: true, subtree: true });
 })();
